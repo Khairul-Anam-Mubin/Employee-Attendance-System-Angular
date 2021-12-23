0432c18578249d005c4fe10acdb65ba085b75fda
Khairul Anam Mubin <anam.mubin@gmail.com>
2021-12-23 18:25:11 +0600

    if no user exist show create admin button on homepage added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
-
+import { SharedService } from './shared.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private service: SharedService) { }
   title = 'EmployeeAttendanceSystem';
+  EmployeeList:any=[];
+  buttonDisabled:any;
+  buttonName = "";
+  ActivateAddEmployeeCom:boolean = false;
+  employee:any;
+  ngOnInit(): void {
+    this.buttonDisabled = true; 
+    this.refreshEmployeeList();
+    if (this.buttonDisabled === false) {
+      this.buttonName = "Create Admin";
+    }
+  }
+  refreshEmployeeList() {
+    this.service.getEmployeeList().subscribe(data=>{
+      this.EmployeeList = data;
+      if (this.EmployeeList.length === 0) this.buttonDisabled = false;
+    });
+  }
+  addClick(){
+    this.employee={
+      EmployeeId:"",
+      EmployeeFirstName:"",
+      EmployeeLastName:"",
+      EmployeeDesignation:"",
+      EmployeeAddress:"",
+      EmployeePhoneNumber:"",
+      EmployeeEmail:"",
+      EmployeePin:""
+    }
+    this.ActivateAddEmployeeCom = true;
+  }
+  closeClick() {
+    this.ActivateAddEmployeeCom= false;
+    this.refreshEmployeeList();
+  }
 }
